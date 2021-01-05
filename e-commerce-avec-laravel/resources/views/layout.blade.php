7d9c1b9d8dad61c379501dd23f381198310731d8
Ferdinand-Lohalo <ferdinandlohalo@gmail.com>
2021-01-05 22:35:44 +0100

    CRUD
    Création de toutes les fonctions Création, Suppression, Mise à jour et Insersion

---
+++ e-commerce-avec-laravel/resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Tutoriel Laravel 8 CRUD</title>
+        <link href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <script src="{{ asset('vendor/bootstrap/js/bootstrap.js') }}" type="text/js"></script>
+    </body>
+</html>
