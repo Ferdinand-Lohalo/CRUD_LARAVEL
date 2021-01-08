39a6aea46bba6f84915bfa3395ebd8551f7a3512
Ferdinand-Lohalo <ferdinandlohalo@gmail.com>
2021-01-08 20:40:04 +0100

    mise à jour

--- e-commerce-avec-laravel/routes/web.php
+++ e-commerce-avec-laravel/routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::resource('cars', 'CarController');
-
+Route::get('create', function (){
+    return view('create');
+});
 Route::get('/', function () {
     return view('welcome');
 });
