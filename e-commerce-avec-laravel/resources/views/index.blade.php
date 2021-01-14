e0848c56a85cafd7e15608dd9c861e2343e8b2e2
Ferdinand-Lohalo <ferdinandlohalo@gmail.com>
2021-01-14 13:36:12 +0100

    Mise à jour
    Changement de la page Indexe du tableau aux card

--- e-commerce-avec-laravel/resources/views/index.blade.php
+++ e-commerce-avec-laravel/resources/views/index.blade.php
@@ -11,41 +11,21 @@
         <h3>Modele des voitures actuellement sur le marché et leurs prix!</h3>
         <hr>
     </div>
-    <div class="uper">
-        @if(session()->get('success')) {{-- condition pour l'affich du mssg success --}}
-            <div class="alert alert-success">
-                {{ session()->get('success') }}
-            </div><br />
-        @endif
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <td>Id</td>
-                    <td>Marque</td>
-                    <td>Prix</td>
-                    <td>Image</td>
-                    <td colspan="2">Action</td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($voitures as $voiture) {{-- condition pour faire afficher les données qui vinent de bd. --}}
-                    <tr>
-                        <td>{{$voiture->id}}</td>
-                        <td>{{$voiture->marque}}</td>
-                        <td>{{$voiture->prix}}</td>
-                        <td> <img src="{{ asset('uploads/car/' .$voiture->image) }}" width="100px"></td> {{-- ce chemin permet d'achicher la photo --}}
-                        <td><a href="{{ route('cars.edit', $voiture->id)}}" class="btn btn-success">Modifier</a></td>
-                        <td>
-                            <form action="{{ route('cars.destroy', $voiture->id)}}" method="post">
-                                @csrf {{-- méthode tocken pour la sécurité des données dans le formulaire --}}
-                                @method('DELETE') {{-- la méthode pour supprimer les fichiers dans la BD --}}
-                                <button class="btn btn-danger" type="submit">Supprimer</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    <div>
+
+<div class="card-deck">
+    <div class="row">
+        @foreach($voitures as $voiture) 
+            <div class="card">
+                <img src="{{ asset('uploads/car/' .$voiture->image) }}" width="100%" class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">{{$voiture->marque}}</h5>
+                <p class="card-text">This is a longer card with.</p>
+                <h4>{{$voiture->prix}}</h4>
+                {{-- <p>Ajouter le <small class="text-muted">{{ $voiture->created_at->format('d/m/Y') }}</small></p> --}}
+                <a href="#" class="btn btn-primary">Detail</a>
+            </div>
+            </div>
+        @endforeach
     </div>
+</div>
 @endsection
